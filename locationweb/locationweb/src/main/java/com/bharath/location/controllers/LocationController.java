a8578b423ab0316ca0471ee3f62f715c63038039
Jyoti Nadda <jyotinadda@gmail.com>
2019-08-28 11:30:07 +0530

    first commit

---
+++ locationweb/locationweb/src/main/java/com/bharath/location/controllers/LocationController.java
@@ -0,0 +1,68 @@
+package com.bharath.location.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bharath.location.entities.Location;
+import com.bharath.location.service.LocationService;
+
+@Controller
+public class LocationController {
+	
+	@Autowired
+	LocationService service;
+	
+	@RequestMapping("/showCreate")
+	public String showCreate()
+	{
+		return "createLocation";
+	}
+	
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location,ModelMap modelMap)
+	{
+		Location locationSaved = service.saveLocation(location);
+		String msg="Location saved with id: "+locationSaved.getId();
+		modelMap.addAttribute("msg",msg);
+		return "createLocation";
+	}
+	
+	@RequestMapping("/displayLocations")
+	public String displayLocations(ModelMap modelMap)
+	{
+		List<Location> locations = service.getAllLocations();
+		modelMap.addAttribute("locations",locations);
+		return "displayLocations";
+	}
+	
+	@RequestMapping("/deleteLocation")
+	public String deleteLocation(@RequestParam("id") int id,ModelMap modelMap)
+	{
+		Location location =new Location();
+		location.setId(id);
+		service.deleteLocation(location);
+		List<Location> locations = service.getAllLocations();
+		modelMap.addAttribute("locations",locations);
+		return "displayLocations";
+	}
+	
+	@RequestMapping("/updateLocation")
+	public String showUpdate(@RequestParam("id") int id,ModelMap modelMap)
+	{
+		Location location = service.getLocationById(id);
+		modelMap.addAttribute("location",location);
+		return "updateLocation";
+	}
+	
+	@RequestMapping("/updateLoc")
+	public String updateLocation()
+	{
+		
+	}
+}
---
+++ locationweb/locationweb/src/main/java/com/bharath/location/repos/LocationRepository.java
@@ -0,0 +1,10 @@
+package com.bharath.location.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bharath.location.entities.Location;
+
+
+public interface LocationRepository extends JpaRepository<Location,Integer> {
+
+}
